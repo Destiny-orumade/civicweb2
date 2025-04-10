0556caa65669c3e0ad2e0c5c2a9249a6a989846b
Mmeso1 <anisiubammesoma8@gmail.com>
2025-04-10 19:02:08 +0100

    fixed the merge errors

--- civicweb02/src/App.tsx
+++ civicweb02/src/App.tsx
@@ -9,13 +9,16 @@ import Signup from "./pages/Signup";
 import DashboardLayout from "./components/Dashboard/Dashboard_Layout";
 import Dashboard from "./pages/Dashboard";
 import "./styles/global.css"; // optional global styles
+import HomePage from "./pages/homePage";
+import AuthModal from "./components/Auth/AuthModal";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       // Every route goes here
       <>
-        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
           {/* <Route path="home" element={<Dashboard />} />
