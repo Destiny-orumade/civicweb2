c568a90ac6c23dc2c8f025dc8fae8eccf70f0770
novice000 <maxwellefemena@gmail.com>
2025-04-10 19:10:48 +0100

    doneee

--- civicweb02/src/pages/Dashboard.tsx
+++ civicweb02/src/pages/Dashboard.tsx
@@ -1,5 +1,13 @@
 const Dashboard = () => {
-  return <div className="bg-transparent">Dashboard</div>;
+  return (
+    <div
+      className="bg-green-700 text-2xl
+   text-white flex flex-col h-screen justify-center items-center"
+    >
+      <p className="text-6xl">Dashboard</p>
+      <p className="animate-ping">Coming soon</p>
+    </div>
+  );
 };
 
 export default Dashboard;
