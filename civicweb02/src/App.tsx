c5ca0b41cd1628293cadc5216e86c3851e05e0ee
novice000 <maxwellefemena@gmail.com>
2025-04-11 01:06:14 +0100

    added

--- civicweb02/src/App.tsx
+++ civicweb02/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homepage";
-import SignUpPage from "./pages/signup";
-import SignInPage from "./pages/signin";
-import LearnMore from "./pages/learn-more";
-import Layout from "./components/shared/layout";
+import HomePage from "@/pages/homepage";
+import SignUpPage from "@/pages/signup";
+import SignInPage from "@/pages/signin";
+import LearnMore from "@/pages/learn-more";
+import Layout from "@/components/shared/layout";
 import DashboardLayout from "./components/dashboard/layout";
 import Dashboard from "./pages/dashboard";
 
